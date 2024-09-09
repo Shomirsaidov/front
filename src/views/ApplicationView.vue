b5353dfc014d976b7212407ca49aeed56bf62c0e
unknown <shomirsaidov.abubakr@mail.ru>
2024-09-09 23:50:09 +0500

    first changes

--- src/views/ApplicationView.vue
+++ src/views/ApplicationView.vue
@@ -39,8 +39,10 @@
                 <div>ул.Гагарина, Худжанд, Таджикистан</div>
             </div>
 
-
-            <div style="height:300px;overflow:hidden;position:relative;"><iframe style="width:100%;height:100%;border:1px solid #e6e6e6;border-radius:8px;box-sizing:border-box" src="https://yandex.ru/maps-reviews-widget/40043229541?comments"></iframe><a href="https://yandex.tj/maps/org/tsentr_perelivaniya_krovi/40043229541/" target="_blank" style="box-sizing:border-box;text-decoration:none;color:#b3b3b3;font-size:10px;font-family:YS Text,sans-serif;padding:0 20px;position:absolute;bottom:8px;width:100%;text-align:center;left:0;overflow:hidden;text-overflow:ellipsis;display:block;max-height:14px;white-space:nowrap;padding:0 16px;box-sizing:border-box">Центр переливания крови на карте Худжанда — Яндекс Карты</a></div>
+            <a href="https://yandex.eu/maps/-/CDDibHke">
+                <img class="rounded-xl border-2 mt-8" src="../assets/map.png" alt="">
+            </a>
+            <!-- <div class="rounded-lg" style="position:relative;overflow:hidden;"><a href="https://yandex.eu/maps/org/gazoyl/179611805457/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Мир шитья Душанбе</a><a href="https://yandex.ru/maps/10318/dushanbe/category/hostel/20699506347/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:14px;">Мир шитья в Душанбе</a><iframe src="https://yandex.ru/map-widget/v1/?ll=68.756584%2C38.512211&mode=search&oid=165422071387&ol=biz&z=17.36" class="w-full" height="300" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe></div> -->
 
 
         </div>
