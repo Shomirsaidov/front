0e5ec20367711ba8ea93d132670e7abedb6bf236
unknown <shomirsaidov.abubakr@mail.ru>
2024-09-09 23:20:07 +0500

    first changes

--- src/views/ApplicationView.vue
+++ src/views/ApplicationView.vue
@@ -40,9 +40,8 @@
             </div>
 
 
-            <!-- <div style="height:300px;overflow:hidden;position:relative;"><iframe style="width:100%;height:100%;border:1px solid #e6e6e6;border-radius:8px;box-sizing:border-box" src="https://yandex.ru/maps-reviews-widget/40043229541?comments"></iframe><a href="https://yandex.tj/maps/org/tsentr_perelivaniya_krovi/40043229541/" target="_blank" style="box-sizing:border-box;text-decoration:none;color:#b3b3b3;font-size:10px;font-family:YS Text,sans-serif;padding:0 20px;position:absolute;bottom:8px;width:100%;text-align:center;left:0;overflow:hidden;text-overflow:ellipsis;display:block;max-height:14px;white-space:nowrap;padding:0 16px;box-sizing:border-box">Центр переливания крови на карте Худжанда — Яндекс Карты</a></div> -->
+            <div style="height:300px;overflow:hidden;position:relative;"><iframe style="width:100%;height:100%;border:1px solid #e6e6e6;border-radius:8px;box-sizing:border-box" src="https://yandex.ru/maps-reviews-widget/40043229541?comments"></iframe><a href="https://yandex.tj/maps/org/tsentr_perelivaniya_krovi/40043229541/" target="_blank" style="box-sizing:border-box;text-decoration:none;color:#b3b3b3;font-size:10px;font-family:YS Text,sans-serif;padding:0 20px;position:absolute;bottom:8px;width:100%;text-align:center;left:0;overflow:hidden;text-overflow:ellipsis;display:block;max-height:14px;white-space:nowrap;padding:0 16px;box-sizing:border-box">Центр переливания крови на карте Худжанда — Яндекс Карты</a></div>
 
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1061.818160207036!2d69.64046606913271!3d40.29106867750322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38b1b3fe8dc555ef%3A0x28fcfae04ee9c9e0!2sTsentr%20Perelivaniya%20Krovi!5e0!3m2!1sen!2s!4v1725903120763!5m2!1sen!2s" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
         </div>
 
